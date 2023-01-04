7f3d9f826f6084b7df79094dd19193410fb9639e
laharika22wh1a1245 <121919979+laharika22wh1a1245@users.noreply.github.com>
2023-01-04 12:10:31 +0530

    Add files via upload

---
+++ laharika.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int c;
+float f;
+printf("/n enter farenheit values");
+scanf("%f",&f);
+c=(f-32)*5/9;
+printf("/n %2f farenheit equal to %d celsius",f,c);
+
+}
+
