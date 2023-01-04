64220547fe96d5e253deb1e9e2825df81cdde20d
laharika22wh1a1245 <121919979+laharika22wh1a1245@users.noreply.github.com>
2023-01-04 12:23:37 +0530

    Add files via upload

---
+++ divisible.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if (a/7==0)
+printf("\n number is divisible by 7");
+if (a/5==0)
+printf("\n number is divisible by 5");
+}
+
