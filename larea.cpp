9b4b69c0b35c2052cc7d4cd50f078d0e6290d9ea
laharika22wh1a1245 <121919979+laharika22wh1a1245@users.noreply.github.com>
2023-01-06 19:34:42 +0530

    Add files via upload

---
+++ larea.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int r;
+	float pie,c;
+	printf("enter the value of r");
+	scanf("%d", &r);
+	printf("enter the value of pie");
+	scanf("%f",&pie);
+c=pie*r*r;
+
+	printf("\n %f",c);
+}
+	
+
